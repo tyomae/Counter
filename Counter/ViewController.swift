c07d5ae13062af29f2d0f71024d92b4b048c1764
Artem <dom1nuk@yandex.ru>
2019-11-02 11:54:10 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Артем  Емельянов  on 02/11/2019.
+//  Copyright © 2019 Artem Emelianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
